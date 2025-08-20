b76813576aebff7c99224b3b3d5f376e6ec5a112
arunvermacsaiml24 <arun.verma_cs.aiml24@gla.ac.in>
2025-08-20 17:06:56 +0530

    Create KmToMiles.java

---
+++ Java Programming Elements Level 1 Lab Practice.java/KmToMiles.java
@@ -0,0 +1,12 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+        // Given distance in kilometers
+        double kilometers = 10.8;
+
+        // Conversion factor: 1 km = 1.6 miles
+        double miles = kilometers * 1.6;
+
+        // Output
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
